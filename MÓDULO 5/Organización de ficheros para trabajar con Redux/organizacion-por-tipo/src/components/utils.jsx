5e411a66f1b6379ba4618b3b0c864ca6b5288d7b
Javier Ramos Nodal <javiramosnodal@gmail.com>
2023-08-08 17:35:28 +0200

    Dispatch.

---
+++ MÓDULO 5/Organización de ficheros para trabajar con Redux/organizacion-por-tipo/src/components/utils.jsx
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+import { ContextoTema } from "../app/App";
+
+export const Boton = (props) => {
+  const tema = useContext(ContextoTema);
+  return (
+    <button
+      style={{ background: tema.fondo2, color: tema.texto }}
+      {...props}
+    ></button>
+  );
+};
