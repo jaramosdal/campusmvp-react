703c1e6e6d13a97dc1a9e3d25ef644c291e3b7e6
Javier Ramos Nodal <javiramos@soliss.com>
2023-09-09 09:52:05 +0200

    He puesto en el login un control para que rediriga a tableros si estás logado.

--- MÓDULO 7/07_avanzadas/070-kanpus-jwt/src/features/sesion/Login.jsx
+++ MÓDULO 7/07_avanzadas/070-kanpus-jwt/src/features/sesion/Login.jsx
@@ -1,45 +1,90 @@
-import { BotonPrimario, CampoInput, Tarjeta } from '../utils/ui'
-import { useInput } from '../utils/hooks'
-import Sesion from './api'
-import { Form, Link, redirect, useActionData } from 'react-router-dom'
-import { store } from '../../app/store'
-import { sesionIniciada } from './authSlice'
+import { BotonPrimario, CampoInput, Tarjeta } from "../utils/ui";
+import { useInput } from "../utils/hooks";
+import Sesion from "./api";
+import { Form, Link, redirect, useActionData } from "react-router-dom";
+import { store } from "../../app/store";
+import { sesionIniciada } from "./authSlice";
+
+// JRN
+export const loader = (params) => {
+  const user = Sesion.getUser();
+
+  if (user) {
+    return redirect("/tableros");
+  } else {
+    return false;
+  }
+};
 
 export const action = async ({ request }) => {
-  const data = await request.formData()
+  const data = await request.formData();
   // Comprobar el usuario y la contraseña
-  const result = await Sesion.login(data.get("usuarioLogin"), data.get("passLogin"))
+  const result = await Sesion.login(
+    data.get("usuarioLogin"),
+    data.get("passLogin")
+  );
 
   if (result) {
-    store.dispatch(sesionIniciada(result))
-    return redirect("/tableros")
+    store.dispatch(sesionIniciada(result));
+    return redirect("/tableros");
   } else {
-    return { inicioFallido: true }
+    return { inicioFallido: true };
   }
-}
+};
 
 const FormularioLogin = () => {
-  const inicioFallido = useActionData()?.inicioFallido
-  const [usuario, setUsuario] = useInput("")
-  const [contrasena, setContrasena] = useInput("")
+  const inicioFallido = useActionData()?.inicioFallido;
+  const [usuario, setUsuario] = useInput("");
+  const [contrasena, setContrasena] = useInput("");
 
   return (
     <Form method="post">
       <fieldset>
-        <Tarjeta 
+        <Tarjeta
           top="Ya tengo una cuenta"
-          bottom={<>
-            <BotonPrimario type="submit" className="w-full">Iniciar sesión</BotonPrimario>
-            {inicioFallido && <p className="text-center text-red-500 pt-3">El usuario o la contraseña son incorrectos.</p>}
-            <p className="pt-3 text-center">¿No tienes una cuenta? <Link to="../crear" className="text-cyan-800 dark:text-cyan-300">Crea una nueva</Link></p>
-          </>}>
-          <CampoInput id="usuarioLogin" type="text" placeholder="alice" required
-            value={usuario} onChange={setUsuario} etiqueta="Nombre de usuario" />
-          <CampoInput id="passLogin" type="password" required value={contrasena} onChange={setContrasena} etiqueta="Contraseña" />
+          bottom={
+            <>
+              <BotonPrimario type="submit" className="w-full">
+                Iniciar sesión
+              </BotonPrimario>
+              {inicioFallido && (
+                <p className="text-center text-red-500 pt-3">
+                  El usuario o la contraseña son incorrectos.
+                </p>
+              )}
+              <p className="pt-3 text-center">
+                ¿No tienes una cuenta?{" "}
+                <Link
+                  to="../crear"
+                  className="text-cyan-800 dark:text-cyan-300"
+                >
+                  Crea una nueva
+                </Link>
+              </p>
+            </>
+          }
+        >
+          <CampoInput
+            id="usuarioLogin"
+            type="text"
+            placeholder="alice"
+            required
+            value={usuario}
+            onChange={setUsuario}
+            etiqueta="Nombre de usuario"
+          />
+          <CampoInput
+            id="passLogin"
+            type="password"
+            required
+            value={contrasena}
+            onChange={setContrasena}
+            etiqueta="Contraseña"
+          />
         </Tarjeta>
       </fieldset>
     </Form>
-  )
-}
+  );
+};
 
-export default FormularioLogin
+export default FormularioLogin;
--- MÓDULO 7/07_avanzadas/070-kanpus-jwt/src/main.jsx
+++ MÓDULO 7/07_avanzadas/070-kanpus-jwt/src/main.jsx
@@ -1,43 +1,76 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app/App'
-import Tablero, { loader as tableroLoader } from './features/tablero/Tablero'
-import './index.css'
-import { Provider } from 'react-redux'
-import { store } from './app/store'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Navigate } from 'react-router-dom'
-import PantallaAcceso from './features/sesion/PantallaAcceso'
-import FormularioRegistro, { action as registroAction } from './features/sesion/Registro'
-import MisTableros from './features/tablero/MisTableros'
-import EdicionListas from './features/tablero/EdicionListas'
-import PanelListas from './features/tablero/PanelListas'
-import { PaginaProtegida, loader as sesionLoader } from './features/sesion/PaginaProtegida'
-import { loader as logoutLoader } from './features/sesion/logout'
-import FormularioLogin, { action as loginAction } from './features/sesion/Login'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app/App";
+import Tablero, { loader as tableroLoader } from "./features/tablero/Tablero";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+import PantallaAcceso from "./features/sesion/PantallaAcceso";
+import FormularioRegistro, {
+  action as registroAction,
+} from "./features/sesion/Registro";
+import MisTableros from "./features/tablero/MisTableros";
+import EdicionListas from "./features/tablero/EdicionListas";
+import PanelListas from "./features/tablero/PanelListas";
+import {
+  PaginaProtegida,
+  loader as sesionLoader,
+} from "./features/sesion/PaginaProtegida";
+import { loader as logoutLoader } from "./features/sesion/logout";
+import FormularioLogin, {
+  action as loginAction,
+  loader as loginLoader,
+} from "./features/sesion/Login";
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path='/' element={<App />} >
-    <Route index element={<Navigate to="cuenta" />} />
-    <Route path='cuenta' element={<PantallaAcceso />}>
-      <Route index element={<Navigate to="acceder" />} />
-      <Route path='acceder' element={<FormularioLogin />}  action={loginAction} />
-      <Route path='crear' element={<FormularioRegistro />} action={registroAction} />
-      <Route path='salir' loader={logoutLoader} />
-    </Route>
-    <Route element={<PaginaProtegida />} loader={sesionLoader} id="sesion">
-      <Route path='tableros' element={<MisTableros />} />
-      <Route path='tableros/:tableroId' element={<Tablero />} loader={tableroLoader}>
-        <Route index element={<PanelListas />} loader={tableroLoader} />
-        <Route path='edit' element={<EdicionListas />} loader={tableroLoader} />
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Navigate to="cuenta" />} />
+      <Route path="cuenta" element={<PantallaAcceso />}>
+        <Route index element={<Navigate to="acceder" />} />
+        <Route
+          path="acceder"
+          element={<FormularioLogin />}
+          action={loginAction}
+          loader={loginLoader}
+        />
+        <Route
+          path="crear"
+          element={<FormularioRegistro />}
+          action={registroAction}
+        />
+        <Route path="salir" loader={logoutLoader} />
+      </Route>
+      <Route element={<PaginaProtegida />} loader={sesionLoader} id="sesion">
+        <Route path="tableros" element={<MisTableros />} />
+        <Route
+          path="tableros/:tableroId"
+          element={<Tablero />}
+          loader={tableroLoader}
+        >
+          <Route index element={<PanelListas />} loader={tableroLoader} />
+          <Route
+            path="edit"
+            element={<EdicionListas />}
+            loader={tableroLoader}
+          />
+        </Route>
       </Route>
     </Route>
-  </Route>
-))
+  )
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
-)
+);
